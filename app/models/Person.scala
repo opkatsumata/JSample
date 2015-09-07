d486e48034fc93366543f2eb947f0b8821aba9f9
opkatsumata <yoshihisa.katsumata@opentone.co.jp>
2015-09-07 16:33:55 +0900

    「好きな数字」項目追加

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -3,12 +3,13 @@ package models
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
-case class Person(age: Int, name: Name, bloodType: Option[String])
+case class Person(age: Int, name: Name, bloodType: Option[String], favoriteNumber: Seq[Int])
 
 object Person {
   implicit val personJsonFormatter: Format[Person] = (
     (__ \ "age").format[Int] and
       (__ \ "name").format[Name] and
-      (__ \ "bloodType").formatNullable[String]
+      (__ \ "bloodType").formatNullable[String] and
+      (__ \ "favoriteNumbers").format[Seq[Int]]
     )(Person.apply, unlift(Person.unapply))
 }
--- test/PersonAPISpec.scala
+++ test/PersonAPISpec.scala
@@ -10,23 +10,31 @@ class PersonAPISpec extends Specification {
     "register person" in new WithApplication {
       val Some(result) = route(FakeRequest(POST, "/api/register",
         FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
-        Json.parse( """{"age":24, "name":{"first":"FirstName", "last":"LastName"}}""")))
+        Json.parse( """{"age":24, "name":{"first":"FirstName", "last":"LastName"}, "favoriteNumbers":[3,17]}""")))
       status(result) mustEqual OK
-      contentAsString(result) mustEqual """{"age":24,"name":{"first":"FirstName","last":"LastName"}}"""
+      contentAsString(result) mustEqual """{"age":24,"name":{"first":"FirstName","last":"LastName"},"favoriteNumbers":[3,17]}"""
     }
 
     "register person with bloodType" in new WithApplication {
       val Some(result) = route(FakeRequest(POST, "/api/register",
         FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
-        Json.parse( """{"age":24, "name":{"first":"FirstName", "last":"LastName"}, "bloodType":"AB"}""")))
+        Json.parse( """{"age":24, "name":{"first":"FirstName", "last":"LastName"}, "bloodType":"X", "favoriteNumbers":[3,17]}""")))
       status(result) mustEqual OK
-      contentAsString(result) mustEqual """{"age":24,"name":{"first":"FirstName","last":"LastName"},"bloodType":"AB"}"""
+      contentAsString(result) mustEqual """{"age":24,"name":{"first":"FirstName","last":"LastName"},"bloodType":"X","favoriteNumbers":[3,17]}"""
+    }
+
+    "display json parse error caused by missing favoriteNumbers" in new WithApplication {
+      val Some(result) = route(FakeRequest(POST, "/api/register",
+        FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
+        Json.parse( """{"age":24, "name":{"first":"FirstName", "last":"LastName"}, "typo!!!":[3,17]}""")))
+      status(result) mustEqual BAD_REQUEST
+      contentAsString(result) mustEqual """{"obj.favoriteNumbers":[{"msg":["error.path.missing"],"args":[]}]}"""
     }
 
     "display json parse error caused by PersonJsonFormatter" in new WithApplication {
       val Some(result) = route(FakeRequest(POST, "/api/register",
         FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
-        Json.parse( """{"typo!!!":24, "name":{"first":"FirstName", "last":"LastName"}}""")))
+        Json.parse( """{"typo!!!":24, "name":{"first":"FirstName", "last":"LastName"}, "favoriteNumbers":[3,17]}""")))
       status(result) mustEqual BAD_REQUEST
       contentAsString(result) mustEqual """{"obj.age":[{"msg":["error.path.missing"],"args":[]}]}"""
     }
@@ -34,7 +42,7 @@ class PersonAPISpec extends Specification {
     "display json parse error caused by NameJsonFormatter" in new WithApplication {
       val Some(result) = route(FakeRequest(POST, "/api/register",
         FakeHeaders(Seq(CONTENT_TYPE -> "application/json")),
-        Json.parse( """{"age":24, "name":{"first":"FirstName", "typo!!!":"LastName"}}""")))
+        Json.parse( """{"age":24, "name":{"first":"FirstName", "typo!!!":"LastName"}, "favoriteNumbers":[3,17]}""")))
       status(result) mustEqual BAD_REQUEST
       contentAsString(result) mustEqual """{"obj.name.last":[{"msg":["error.path.missing"],"args":[]}]}"""
     }
